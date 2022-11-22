1a7195f68f96e4dbf573278487dcbdbcd39e66f2
korgi <igrok12005@yandex.ru>
2022-11-22 22:30:32 +0300

    переделал карточку товара под jsx но в ней всё слетело!!

--- components/cart/Cart.tsx
+++ components/cart/Cart.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import styles from "./Cart.module.css";
 import IconStar from "./element/Star.svg";
-
+import IconCompare from "./element/scales.svg";
+import IconLike from "./element/like.svg";
+import { Button } from "../Button/Button";
 
 function createStar(countStar:number){
     for(let i=0;i<countStar;i++){
@@ -8,18 +11,32 @@ function createStar(countStar:number){
     }
 }
 
-const Cart =({product}:any)=>{//на продукт пока заглушка any 
-    const {id, title, rank, img, price}=product;
-
+ export const Cart =() :JSX.Element=>{//на продукт пока заглушка any 
+    
     return(
-        <div>
+        
+        <div className="first">
             <div className="imsec">
-            <img src={img} className="image"/>
+                <img src="https://www.zlatmax.ru/images/zik-0321/01s.webp" className="image"/>
             </div>
-            <span>{title}</span>
-            {createStar(rank)}
-              
-            
+            <span  className="textName">asd</span>
+            <div>
+            <div>
+                <div className="textSize">sizeXsize</div>
+                <div className="textMaterial">Material</div>
+            </div>
+            <IconStar className="star"/>
+            <IconStar className="star"/>
+            <div className="textFeedback">1 Отзыв</div>
+            </div>
+            <div className="divLine">
+                <hr className="line"/>
+            </div>
+            <div>
+            <div className="price">500 р.</div>
+            <div className="imageTwo"><IconCompare className="scale" /><IconLike className="like" /></div>
+            <Button appearance={'primary'}>В корзину</Button>
+            </div>
         </div>
     );
 }
