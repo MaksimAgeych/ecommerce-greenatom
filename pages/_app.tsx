a8c80f6d4e595c3fca52395b766fdb26c00c5a28
Aleksey Mikhailenko <maks.quantum@yandex.ru>
2022-11-22 23:08:14 +0300

    hot redux fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
+import { Provider } from 'react-redux';
+import { store } from '../store/rootReducer';
 
 export default function App({Component, pageProps}: AppProps): JSX.Element {
-    return <>
+    return <Provider store={store}>
         <Component {...pageProps} />
-    </>
+    </Provider>
         ;
 }
