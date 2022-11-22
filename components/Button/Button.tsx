81614a269a9144bb6b9567517fec06e4b9edfed4
Aleksey Mikhailenko <maks.quantum@yandex.ru>
2022-11-22 12:01:15 +0300

    merge conflicts

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import styles from './Button.module.css';
 
-
-const Button = ({children}) => {
+export const Button = ({children, arrow = 'none', appearance, className, ...props}: ButtonProps): JSX.Element => {
     return (
-        <button>
+        <motion.button
+            whileHover={{scale: 1.05}}
+            className={cn(styles.button, className, {
+                [styles.primary]: appearance == 'primary',
+                [styles.ghost]: appearance == 'ghost',
+            })}
+            {...props}
+        >
             {children}
-        </button>
+            {arrow != 'none' && <span className={styles.arrow, {
+                [styles.down]: arrow == 'down',
+                [styles.right]: arrow == 'right'
+            })}> {<ArrowIcon/>} </span>}
+        </motion.button>
     );
 };
-
-export default Button;
---
+++ components/sign-in-form copy/SignInForm.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useState } from 'react';
+import { createUserDocFromAuth, signInWithGooglePopup, signIn } from '../../utils/firebase/firebase.utils';
+import FormInput from '../form-input/FormInput';
+import './sign-in-form.styles.scss';
+import Button from '../button/Button';
+
+const defaultFormFields = {
+    email: '',
+    password: '',
+}
+
+const SignInForm = () => {
+
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const { email, password, } = formFields;
+
+    const handleOnChange = (event) => {
+        const { name, value } = event.target;
+        setFormFields({ ...formFields, [name]: value })
+    }
+
+    const signInWithGoogle = async () => {
+        const { user } = await signInWithGooglePopup()
+        // setCurrentUser(user)
+        await createUserDocFromAuth(user);
+
+    }
+
+    const handleOnSubmit = async (event) => {
+        event.preventDefault();
+
+        const { email, password } = formFields;
+
+        try {
+            const { user } = await signIn(email, password);
+            console.log(user)
+            setFormFields(defaultFormFields); //очистка полей при успешной авторизцации
+        } catch (error) {
+
+            switch (error.code) {
+                case 'auth/wrong-password':
+                    return alert('Wrong Password')
+                case 'auth/user-not-found':
+                    return alert('Incorrect email OR user does not exists');
+                default:
+                    return;
+            }
+        }
+    }
+    return (
+        <div className='sign-up-container'>
+            <h2>Already have an account?</h2>
+            <span> SIGN IN with email and password</span>
+
+            <form onSubmit={handleOnSubmit} >
+
+                <FormInput label={'Email'} type='email' required onChange={handleOnChange} name='email' value={email} />
+
+                <FormInput label={'Password'} type='password' required onChange={handleOnChange} name='password' value={password} />
+
+                <div className='button-wrapper'>
+                    <Button type='submit' buttonType='default' onSubmit={handleOnSubmit} >SIGN IN</Button>
+                    <Button type='submit' buttonType='google' onSubmit={signInWithGoogle} >SIGN IN WITH GOOGLE</Button>
+
+                </div>
+            </form>
+        </div >
+    );
+};
+
+export default SignInForm;
