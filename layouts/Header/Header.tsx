fa05e89e5084c5d7ca2aed7dd25853e63d6e647f
n1k0lay <kolia.d97@gmail.com>
2022-11-23 00:25:48 +0500

    Закончена верстка меню. Правка svg. Добавлен _document для next.js.

--- layouts/Header/Header.tsx
+++ layouts/Header/Header.tsx
@@ -5,14 +5,18 @@ import cn from 'classnames';
 import Link from "next/link";
 import Logo from "../logo.svg";
 import {Search} from "../../components";
+import IconLocate from './locate.svg';
+import IconFav from './fav.svg';
+import IconCart from './cart.svg';
+import IconAuth from './auth.svg';
 
 
 export const Header = ({className, ...props}: HeaderProps): JSX.Element => {
 
     return (
         <header className={cn(className, styles.header)} {...props}>
-            <div className={styles.topHeader}>
-                <div>
+            <div className={cn(styles.container, styles.black)}>
+                <div className={styles.topHeader}>
                     <div className={styles.leftTopHeader}>
                         <ul>
                             <li><Link href={'/'}>О нас</Link></li>
@@ -22,15 +26,50 @@ export const Header = ({className, ...props}: HeaderProps): JSX.Element => {
                         </ul>
                     </div>
                     <div className={styles.rightTopHeader}>
-                        <Link href={'/'}>Личный Кабинет</Link>
+                        <Link className={styles.lk} href={'/auth'}>
+                            <IconAuth className={styles.iconAuth}/>
+                            <span>Личный Кабинет</span>
+                        </Link>
                     </div>
                 </div>
             </div>
-            <div className={styles.midHeader}>
-                <Logo />
-                <Search />
+            <div className={cn(styles.container, styles.gray)}>
+                <div className={styles.midHeader}>
+                    <Link href={'http://localhost:3000/'}><Logo/></Link>
+                    <Search/>
+                    <div className={styles.locate}>
+                        <IconLocate/>
+                        <span> Москва</span>
+                    </div>
+                    <div className={styles.contact}>
+                        <Link className={styles.tel} href={'tel:8800000000'}>8-800-000-000</Link>
+                        <Link className={styles.callBack} href={'/'}>Заказать звонок</Link>
+                    </div>
+                    <div className={styles.favorites}>
+                        <Link href={'/favorites'}><IconFav/></Link>
+                    </div>
+                    <div className={styles.cart}>
+                        <IconCart/>
+                        <div className={styles.cardData}>
+                            <span className={styles.cartAmount}>0 р.</span>
+                            <Link className={styles.checkout} href={'/cart'}>Оформить заказ</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className={styles.botHeader}>bot</div>
+            <div className={cn(styles.container, styles.black)}>
+                <div className={styles.botHeader}>
+                    <nav className={styles.menu}>
+                        <ul>
+                            <li><Link href={'/'}>Каталог ножей</Link></li>
+                            <li><Link href={'/'}>Клинковое оружие</Link></li>
+                            <li><Link href={'/'}>Сувенирные изделия</Link></li>
+                            <li><Link href={'/'}>Фонари ARMYTEK</Link></li>
+                            <li><Link href={'/'}>Сопутствующие товары</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </header>
     );
 };
