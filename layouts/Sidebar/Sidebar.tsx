b2bf4ac6068d8b506e736313a15eaa51de4cedf3
n1k0lay <kolia.d97@gmail.com>
2022-11-24 20:34:12 +0500

    Добавлены виджеты для теста

--- layouts/Sidebar/Sidebar.tsx
+++ layouts/Sidebar/Sidebar.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import styles from './Sidebar.module.css';
 import {SidebarProps} from "./Sidebar.props";
 import cn from "classnames";
+import {SidebarWidget} from "../../components";
 
 export const Sidebar = ({className, ...props}: SidebarProps): JSX.Element => {
     return (
         <div {...props} className={cn(className, styles.sidebar)}>
-            Сайдбар
+            <SidebarWidget name={"Цена"}>Контент</SidebarWidget>
+            <SidebarWidget name={"Категории"}>Контент</SidebarWidget>
+            <SidebarWidget name={"Длина ножа"}>Контент</SidebarWidget>
         </div>
     );
 };
