3657c64bd7790a57da74ec9015e10392f430150e
MaksimAgeych <maksimageych@gmail.com>
2022-11-25 16:23:47 +0300

    Менерес каталог продуктов в компоненты

--- layouts/Menu/Menu.tsx
+++ layouts/Menu/Menu.tsx
@@ -1,28 +1,9 @@
-import React, { useEffect } from 'react';
-import { useAppSelector } from '../../hooks/redux-hooks';
-import { RootState } from '../../store/rootReducer';
 import styles from './Menu.module.css';
-import { useAppDispatch } from './../../hooks/redux-hooks';
-import { ProductCart } from './../../components/ProductCard/ProductCart';
-import { fetchAllProducts } from '../../store/productsAsyncActions';
 
-export const Menu = (): JSX.Element => {
-    const {products, status} = useAppSelector((state) => state.products);
-    const dispatch = useAppDispatch();
-
-    useEffect(() => {
-        dispatch(fetchAllProducts());
-    }, []);
-
+export const Menu = () => {
     return (
-        <nav className={styles.menu} role={"navigation"}>
-            {status === "loading" && <div>Загрузка</div>}
-            {status === "error" && <div>Ошибка</div>}
-            {
-                products.map((product) => (
-                    <ProductCart key={product.id} {...product}/>
-                ))
-            }
-        </nav>
-    );
-};
+        <div>
+            Меню
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 import {Htag, P, Button} from "../components";
 import {withLayout} from "../layouts/Layout";
 import Link from "next/link";
-import { Menu } from './../layouts/Menu/Menu';
+import { ProductsCatalog } from '../components/ProductsCatalog/ProductsCatalog';
 
 
 function Home() {
@@ -13,7 +13,7 @@ function Home() {
       <Link href={'/cart'}>Корзина</Link>
       <Link href={'/catalog'}>Каталог</Link>
       <Link href={'/favorites'}>Избранное</Link> */}
-      <Menu />
+      <ProductsCatalog />
     </div>
 
 
