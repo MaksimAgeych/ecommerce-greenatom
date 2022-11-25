00683b31d1dd151a0a4b93bce674afd1fb316f53
n1k0lay <kolia.d97@gmail.com>
2022-11-25 21:07:50 +0500

    Удаление стилей

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,10 @@
-
-import styles from '../styles/Home.module.css'
-import {Htag, P, Button} from "../components";
 import {withLayout} from "../layouts/Layout";
-import Link from "next/link";
 import { ProductsCatalog } from '../components/ProductsCatalog/ProductsCatalog';
 
 
 function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       {/* <Link href={'/auth'}>Авторизация</Link>
       <Link href={'/cart'}>Корзина</Link>
       <Link href={'/catalog'}>Каталог</Link>
