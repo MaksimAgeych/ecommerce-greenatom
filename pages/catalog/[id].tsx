763857e4a1800b9e1f4f5f9458c778fa29e2968a
n1k0lay <kolia.d97@gmail.com>
2022-11-26 00:09:16 +0500

    Добавлен SSG. Закомментирован SSR

--- pages/catalog/[id].tsx
+++ pages/catalog/[id].tsx
@@ -1,34 +1,24 @@
 import React from 'react';
-import {withLayout} from "../../layouts/Layout";
 import {IProduct} from "../../interface/entities/interface";
-import {GetServerSideProps, GetStaticPaths, GetStaticProps, GetStaticPropsContext} from "next";
-import {ParsedUrlQuery} from "querystring";
-import {Htag} from "../../components";
+import {Htag, P} from "../../components";
+import {withLayout} from "../../layouts/Layout";
 
+//SSG
+export const getStaticPaths = async () => {
+    const response = await fetch('http://localhost:4000/products');
+    const data: IProduct[] = await response.json();
 
-// export const getStaticPatch: GetStaticPaths = async () => {
-//     const response = await fetch('http://localhost:4000/products');
-//     const data:IProduct[] = await response.json();
-//
-//     const paths = data.map(({id}) => ({
-//         params: {id: id.toString()},
-//     }));
-//
-//     return {
-//         paths,
-//         fallback: true
-//     };
-// }
+    const paths = data.map(({id}) => ({
+        params: {id: id.toString()},
+    }));
 
+    return {
+        paths,
+        fallback: false
+    };
+}
 
-export const getServerSideProps = async (context: { params: { id: any; }; }) => {
-
-    console.log(context)
-    if (!context.params) {
-        return {
-            noFound: true,
-        }
-    }
+export const getStaticProps = async (context: { params: { id: any; }; }) => {
     const {id} = context.params;
     const response = await fetch(`http://localhost:4000/products/${id}`);
     const data = await response.json();
@@ -36,21 +26,35 @@ export const getServerSideProps = async (context: { params: { id: any; }; }) => 
     return {
         props: {product: data}
     }
-
 }
 
+//SSR - не удалять
+// export const getServerSideProps = async (context: { params: { id: number; }; }) => {
+//     console.log(context)
+//     if (!context.params) {
+//         return {
+//             noFound: true,
+//         }
+//     }
+//     const {id} = context.params;
+//     const response = await fetch(`http://localhost:4000/products/${id}`);
+//     const data = await response.json();
+//     console.log(data)
+//     return {
+//         props: {product: data}
+//     }
+// }
 
 
-// @ts-ignore
-function ProductPage({product}, ...props: any): JSX.Element {
-    // let {id, name} = props.product
-    console.log(product)
+function ProductPage({product}: { product: IProduct }): JSX.Element {
+    let {id, name, description, about, rating, size, price, img} = product;
+    console.log(product.name)
     return (
-        <div>
-            <Htag tag={'h1'}>Товар</Htag>
-            Товар
-        </div>
+        <>
+            <Htag tag={'h1'}>{name}</Htag>
+            <P>{description}</P>
+        </>
     );
 }
 
-export default ProductPage;
+export default withLayout(ProductPage);
