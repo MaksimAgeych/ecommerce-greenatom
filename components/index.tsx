6dd19e689cca2a179f506a64b8ceaf7838907eaf
n1k0lay <kolia.d97@gmail.com>
2022-11-27 11:15:51 +0500

    Правка названий ProductDescription

--- components/index.tsx
+++ components/index.tsx
@@ -9,4 +9,4 @@ export * from './Authentication/Authentication';
 export * from './FormInput/FormInput';
 export * from './SidebarWidget/SidebarWidget';
 export * from './ProductCard/ProductCart';
-export * from './productDescription/ProductDescription';
+export * from './ProductDescription/ProductDescription';
