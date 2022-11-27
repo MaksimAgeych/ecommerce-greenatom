e7acdb02e8c5a9352df7db4f476f2dbfecf7bc15
n1k0lay <kolia.d97@gmail.com>
2022-11-27 21:53:12 +0500

    Верстка страницы "Избранное"

--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -1,48 +1,36 @@
-import { getDocs, getDoc, doc } from 'firebase/firestore';
-import { useEffect } from 'react';
-import { isTemplateMiddle } from 'typescript';
-import { ProductCard } from '../components';
-import { useAppSelector } from '../hooks/redux-hooks';
+import {getDocs, getDoc, doc} from 'firebase/firestore';
+import {useEffect} from 'react';
+import {isTemplateMiddle} from 'typescript';
+import {Htag, ProductCard, ProductCardFav} from '../components';
+import {useAppSelector} from '../hooks/redux-hooks';
 import {withLayout} from '../layouts/Layout';
-import { createUsersProuctDataFromAuth, getProductById, db, newGetDoc } from '../utils/firebase/firebase.utils';
-
+import {createUsersProuctDataFromAuth, getProductById, db} from '../utils/firebase/firebase.utils';
 
 function Favorites(): JSX.Element {
-    
+
     const favProducts = useAppSelector(state => state.favoriets.favoriets)
     const userID = useAppSelector(state => state.user.id)
-  
+
 
     const getFavID = favProducts.map((item) => item.id)
 
-    useEffect( () => {
-    if (userID) async () => {
-        const res = await getProductById(userID, 'users') 
-        // console.log(res)
-        }
-        console.log(userID)
-    }, [userID])
-    
-   
+    // useEffect(() => {
+    //     if (userID) async () => {
+    //         const res = await getProductById(userID, 'users')
+    //         // console.log(res)
+    //     }
+    //     console.log(userID)
+    // }, [userID])
 
+    //TODO: Сделать редирект на авторизацию, если не залогинен
     return (
         <div>
-            Избранное
+            <Htag tag={'h1'}>Избранное</Htag>
 
-            {userID ? 
-            <ul>
-                {
-                    favProducts.map((item) => {
-                return <li key={item.id+item.price}>
-                    <ProductCard item={item} />
-                </li>
-                
-            }) 
-                }
-            </ul>
-           
-            : 'Пользователь не авторизирован'
-        }
+            {userID
+                ? favProducts.map((item) => {return <ProductCardFav item={item} key={item.id}/>})
+                : 'Авторизуйтесь, чтобы получит доступ к вашему списку избранного'
+            }
         </div>
     )
 }
