714d73f5cc41de08b24f03a8d17187b49efa92b5
navys00 <73278416+navys00@users.noreply.github.com>
2022-11-28 17:31:02 +0300

    добавил кнопку в корзину

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from  './Card.module.css';
 
+
 export const Card = (): JSX.Element => { 
     return (
         <section className={styles.sectionCart}>
@@ -12,6 +13,27 @@ export const Card = (): JSX.Element => {
                 </div>
             </div>
             <div className={styles.sectionCartBody}>
+<<<<<<< HEAD
+                    <div className={styles.container}>
+                        <div className={styles.cart}>
+                            <div className={styles.cartHeader}>
+                                <div className={styles.cartHeaderTitle}>товар</div>
+                                <div className={styles.cartHeaderPrice}>цена</div>
+                                <div className={styles.cartHeaderCount}>количество</div>
+                                <div className={styles.cartHeaderCost}>сумма</div>           
+                            </div>
+                                <div className={styles.product}>
+                                    <div className={styles.wrapper}>
+                                        Нож Бекас
+                                    </div>
+                                </div>
+                                <div className={styles.cartFooter}>
+                                    <div className={styles.Buttonstyle}>
+                                        Оформить заказ
+                                    </div>
+                                    итого:
+                                </div>
+=======
                 <div className={styles.container}>
                     <div className={styles.cart}>
                         <div className={styles.cartHeader}>
@@ -19,6 +41,7 @@ export const Card = (): JSX.Element => {
                             <div className={styles.cartHeaderPrice}>цена</div>
                             <div className={styles.cartHeaderCount}>количество</div>
                             <div className={styles.cartHeaderCost}>сумма</div>           
+>>>>>>> 402a497b035ccf625ddd897d1f1fb0d20906cd77
                         </div>
                             <div className={styles.product}>
                                 <div className={styles.wrapper}>
