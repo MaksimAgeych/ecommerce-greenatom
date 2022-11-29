0fa2f040d3b5096670a111213949c9b992d2d63a
n1k0lay <kolia.d97@gmail.com>
2022-11-29 22:55:41 +0500

    Правка ошибки Layout

--- layouts/Layout_LeftSidebar.tsx
+++ layouts/Layout_LeftSidebar.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent} from 'react';
+import React, {FunctionComponent, useEffect} from 'react';
 import styles from './Layout.module.css';
 import {LayoutProps} from "./Layout.props";
 import {Header} from './Header/Header';
@@ -7,7 +7,7 @@ import {AppContextProvider, IAppContext} from "../context/app.context";
 import {Provider} from 'react-redux';
 import {store} from '../store/rootReducer';
 import {Sidebar} from "./Sidebar/Sidebar";
-import {collection, getDocs, query} from "firebase/firestore";
+import {collection, DocumentData, getDocs, query} from "firebase/firestore";
 import {auth,db, getCollectionByName} from "../utils/firebase/firebase.utils";
 import {useCollectionData} from "react-firebase-hooks/firestore";
 import {useAuthState} from "react-firebase-hooks/auth";
@@ -17,25 +17,28 @@ import {IProduct} from "../interface/entities/interface";
 import internal from "stream";
 import firebase from "firebase/compat";
 import FirestoreError = firebase.firestore.FirestoreError;
+import {converter} from "../pages/catalog/[id]";
 
 export const Layout = ({children}: LayoutProps): JSX.Element => {
-
 
     interface IProductDataProps {
         fetchProd: IProduct[];
         loadingProd: boolean;
         errorProd:  FirestoreError | undefined;
     }
-    const q = query(collection(db, 'products',))
-    // @ts-ignore
-    const [fetchProd, loadingProd, errorProd] = useCollectionData<IProductDataProps>(q)
+    const q = query(collection(db, 'products').withConverter(converter))
+
+    const [fetchProd, loadingProd, errorProd] = useCollectionData(q)
     const [user, loadingUser, errorUser] = useAuthState(auth);
     const dispatch = useAppDispatch();
     let products = useAppSelector(state => state.products.products);
-    if (fetchProd) {
-        // @ts-ignore
-        dispatch(addProducts(fetchProd));
-    };
+
+    useEffect(() => {
+        if (fetchProd) {
+            dispatch(addProducts(fetchProd));
+        }
+    }, [fetchProd])
+
 
     if (loadingProd || loadingUser) {
         return (
@@ -55,6 +58,17 @@ export const Layout = ({children}: LayoutProps): JSX.Element => {
             </div>
         );
     }
+
+    // return (
+    //     <div className={styles.wrapperLeftSidebar}>
+    //         <Header className={styles.header}/>
+    //         <Sidebar className={styles.sidebar}/>
+    //         <main className={styles.body} tabIndex={0} role={"main"}>
+    //             {children}
+    //         </main>
+    //         <Footer className={styles.footer}/>
+    //     </div>
+    // );
 };
 
 export const withLayout_leftSidebar = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
@@ -68,7 +82,5 @@ export const withLayout_leftSidebar = <T extends Record<string, unknown> & IAppC
                 </AppContextProvider>
             </Provider>
         );
-
-
     };
 };
