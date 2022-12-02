954a1e324e34a7ad02b5d9cda4fb5f80a234c7c9
Aleksey Mikhailenko <maks.quantum@yandex.ru>
2022-12-02 18:58:42 +0300

    fetching firebase

--- components/Authentication/Authentication.tsx
+++ components/Authentication/Authentication.tsx
@@ -13,7 +13,7 @@ import {useEffect, useState} from "react";
 import {useAppDispatch, useAppSelector} from "../../hooks/redux-hooks";
 import {addManyBasket, getBasket} from "../../store/basketSlice";
 import {addFav, addManyFav, getFavorites} from "../../store/favoritesSlice";
-import { collection, doc, query, QueryDocumentSnapshot, setDoc } from 'firebase/firestore';
+import { collection, doc, getDocs, query, QueryDocumentSnapshot, setDoc } from 'firebase/firestore';
 import { useCollectionData, useCollectionDataOnce } from 'react-firebase-hooks/firestore';
 import { IProduct } from '../../interface/entities/interface.js';
 
@@ -31,44 +31,40 @@ const Authentication = () => {
         const basket = useAppSelector(getBasket)
         const fav = useAppSelector(getFavorites)
 
-        const [currentUserID, setCurrentUserID] = useState<string>('falseUser') 
-        const [usersFavData] = useCollectionDataOnce(collection(db, 'users', currentUserID, 'fav').withConverter(converter))
-        const [usersBasketData] = useCollectionDataOnce(collection(db, 'users', currentUserID, 'basket').withConverter(converter))
+        // const [currentUserID, setCurrentUserID] = useState<string>('falseUser') 
+        // const [usersFavData, loading, error] = useCollectionDataOnce(query(collection(db, 'users', user?.uid || 'falseUser', 'fav').withConverter(converter)))
+        // const [usersBasketData] = useCollectionDataOnce(collection(db, 'users', user?.uid || 'falseUser' , 'basket').withConverter(converter))
         const dispatch = useAppDispatch()
 
 
         //==========Merge user's 
 
-     
-useEffect(() => {
-if (!!user) {
-    const favQUeryPath = (id: string) =>  doc(db, 'users', user.uid.toString(), 'fav', id)
-    const basketQUeryPath = (id: string) =>  doc(db, 'users', user.uid.toString(), 'basket', id)
+     console.log(user)
 
-    basket.forEach((item) => {
-        setDoc(basketQUeryPath(item.id.toString()), item)
-    })
+if (!!user && user.uid !== 'falseUser') {
+    // const favQUeryPath = (id: string) =>  doc(db, 'users', user.uid.toString(), 'fav', id)
+    // const basketQUeryPath = (id: string) =>  doc(db, 'users', user.uid.toString(), 'basket', id)
 
-    setCurrentUserID(user.uid.toString());
-    if (usersBasketData && currentUserID !== 'falseUser') {
-        let mergedBasket = new Set([...usersBasketData, ...basket])
-        dispatch(addManyBasket(Array.from(mergedBasket)))
-    }
-} else {
-    setCurrentUserID('falseUser')
+  
+
+    (async () => {
+          const q = query(collection(db, 'users', user?.uid, 'fav').withConverter(converter)) 
+//   .then((response) => response.forEach((doc) => console.log(doc.data())))
+  
+  const querySnapshot = await getDocs(q);
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, " => ", doc.data());
+});
+    })()
+
+    // // // setCurrentUserID(user.uid.toString());
+    // // if (usersBasketData && currentUserID !== 'falseUser') {
+    //     let mergedBasket = new Set([...usersBasketData, ...basket])
+    //     dispatch(addManyBasket(Array.from(mergedBasket)))
+    // // }
+
 }
-
-})
-  
-    
-    
-  
-    // const {usersFavData} = await getCollectionByName(`users/${user.uid}/fav`);
-    // const {userBasektData} = await getCollectionByName(`users/${user.uid}/basket`)
-
-  
-      
-
         
 
     
